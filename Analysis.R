a01e20ad1f351a2912c953fb97a24d46b337a2ba
justinhjy1004 <justinhjy1004@gmail.com>
2023-12-27 08:21:26 -0500

    changes

--- Analysis.R
+++ Analysis.R
@@ -6,6 +6,7 @@
 
 library(tidyverse)
 library(stargazer)
+library(kableExtra)
 
 df <- read_csv("data.csv")
 # Make it to percentage
@@ -15,9 +16,30 @@ df$diff_growth <- 100*df$diff_growth
 lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), df)
 summary(lmod)
 
+#===============================================================
+# Sensitivity Analysis
+#===============================================================
+
+lmod <- lm(switch ~ diff_growth, df)
+summary(lmod)
+
+lmod <- lm(switch ~ diff_growth + log1p(num_citation), df)
+summary(lmod)
+
+lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), df)
+summary(lmod)
+
+lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year) + as.factor(prev_cpc_section) , df)
+summary(lmod)
+
+#===============================================================
+# Logit and Probit
+#===============================================================
+
 logit <- glm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), 
              family = binomial(link = "logit"), 
              data = df)
+
 summary(logit)
   
 probit <- glm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), 
@@ -26,62 +48,16 @@ probit <- glm(switch ~ diff_growth + log1p(num_citation) + as.factor(year),
 summary(probit)
 
 #===============================================================
-# Regression by Number of Patents
+# Regression by Net Citation and Net Patents
 #===============================================================
 
-# Classify the assignee has having the top 25% in number of patents
-# or bottom 75%
 df |>
-  select(assignee_id, num_patents) |>
-  distinct() -> a
+  ungroup() |>
+  group_by(assignee_id) |>
+  mutate(total_citation_count = sum(num_citation)) -> df
 
-top_25_patent <- quantile(a$num_patents)[[4]]
-
-df |>
-  mutate(top_25_patent = ifelse(num_patents > top_25_patent, T, F)) -> df
-
-# Regression Estimates on the bottom 75% 
-lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), df[!df$top_25_patent,])
+lmod <- lm(switch ~ diff_growth*log1p(num_patents) + log1p(num_citation) + as.factor(year) , df)
 summary(lmod)
 
-# Regression Estimates on the top 25% 
-lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), df[df$top_25_patent,])
-summary(lmod)
-
-# Regression Estimates on interaction
-lmod <- lm(switch ~ diff_growth + log1p(num_citation)*top_25_patent + as.factor(year), df)
-summary(lmod)
-
-#===============================================================
-# Regression by Number of Citations Received
-#===============================================================
-
-# Count the number of total citations received
- df |>
-   select(assignee_id, num_citation) |>
-   group_by(assignee_id) |>
-   summarise(citation_received = sum(num_citation)) -> c
-
-# Get the top 25% of citation receipients
-top_25_citation <- quantile(c$citation_received)[[4]]
-
-c |>
-  mutate(top_citation = ifelse(citation_received > top_25_citation, T, F)) -> c
-
-top_25_producers <- c[c$top_citation,]$assignee_id
-
-# Classify them if they are top of not
-df |>
-  mutate(top_25_citation = ifelse(assignee_id %in% top_25_producers, T, F)) -> df
-
-# Regression Estimates on the top 25% 
-lmod <- lm(switch ~ diff_growth + log1p(num_citation), df[df$assignee_id %in% top_25_producers,])
-summary(lmod)
-
-# Regression Estimates on the bottom 75% 
-lmod <- lm(switch ~ diff_growth + log1p(num_citation), df[!(df$assignee_id %in% top_25_producers),])
-summary(lmod)
-
-# Regression Estimates on the interaction
-lmod <- lm(switch ~ diff_growth + log1p(num_citation)*top_25_citation, df)
+lmod <- lm(switch ~ diff_growth*log1p(total_citation_count) + log1p(num_citation) + as.factor(year) , df)
 summary(lmod)
