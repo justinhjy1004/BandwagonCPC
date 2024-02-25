c4393ff5b7dff4cb6a1f199dbedabcab0908245b
justinhjy1004 <justinhjy1004@gmail.com>
2024-02-25 09:18:34 -0500

    added effect size

--- Analysis.R
+++ Analysis.R
@@ -12,9 +12,26 @@ df <- read_csv("data.csv")
 # Make it to percentage
 df$diff_growth <- 100*df$diff_growth 
 
-# Vanilla Regression
+years <- df |>
+  filter( year >= 1985 ) |>
+  group_by(year) |>
+  summarize( count = n() ) |>
+  mutate( weight = count/nrow(df))
+
+## Vanilla Regression
 lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), df)
 summary(lmod)
+
+## Effect Size Analysis
+average_year_effect <- sum(lmod$coefficients[-c(2,3)] * years$weight)
+average_citation_effect <- mean(log1p(df$num_citation)) * lmod$coefficients[["log1p(num_citation)"]]
+
+diff_growth <- quantile(df$diff_growth, probs = seq(0, 1, by = 0.001), na.rm = T)
+
+y <- (diff_growth * lmod$coefficients[["diff_growth"]]) + average_year_effect + average_citation_effect
+
+ggplot(mapping = aes(x=diff_growth, y = y)) +
+  geom_point()
 
 #===============================================================
 # Sensitivity Analysis
@@ -30,6 +47,9 @@ lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year), df)
 summary(lmod)
 
 lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year) + as.factor(prev_cpc_section) , df)
+summary(lmod)
+
+lmod <- lm(switch ~ diff_growth + log1p(num_citation) + as.factor(year) + as.factor(prev_cpc_section) + as.factor(cpc_section) , df)
 summary(lmod)
 
 #===============================================================
